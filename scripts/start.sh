de4ce444c96ad454c2935294320fe809a06ca017
alpha <rajudawadinp@gmail.com>
2017-09-01 18:16:21 +0545

    initial commit for php 7.0 with ubuntu 16.04 docker

---
+++ scripts/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+# Set the right permissions (needed when mounting from a volume)
+chown -Rf www-data.www-data /var/www/
+
+# Start supervisord and services
+/usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
+
+exec $@
